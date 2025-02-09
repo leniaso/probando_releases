569b98469887b7f147089cfe0b3477b0d0ded148
leniaso <alejandro.naranjoc@udea.edu.co>
2025-02-08 19:02:03 -0500

    primer release

---
+++ setub.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="probando_releases",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="leniaso",                         # Tu nombre
+    author_email="alejandro.naranjoc@udea.edu.co",                 # Tu correo electrónico
+    url="https://github.com/leniaso/probando_releases",     # URL del proyecto
+)
